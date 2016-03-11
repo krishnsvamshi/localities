96fddd10d0259f612a4ca6817f7b6e89babb0814
Vamshi <vamshikrishnagoud.m@gmail.com>
2016-03-11 19:21:53 +0530

    fhgh

--- app/views/gitNotify.scala.js
+++
@@ -1,6 +0,0 @@
-@(gitnotification:models.GitNotification)
-<script type="text/javascript" charset="utf-8" src="@routes.RepositoryController.chatRoom()"></script >  
-<script>
-var nid = '@gitnotification.id';
-alert(nid);
-</script>
