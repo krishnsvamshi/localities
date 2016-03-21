b6ab6bae8696b4487531fe88ac4dedffb86e0199
Vamshi <vamshikrishnagoud.m@gmail.com>
2016-03-21 18:03:33 +0530

    test commit 2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,7 +20,6 @@ public class Application extends Controller {
     public Result index() {
  
         return ok(index.render("Your new application is ready."));
-        
     }
    public Result getSource(String state){
 	   String urlString = "";
