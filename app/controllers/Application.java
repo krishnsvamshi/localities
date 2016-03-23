0d844fd96fcde2ff6b186650298a7bcea72b3f6f
Vamshi <vamshikrishnagoud.m@gmail.com>
2016-03-23 11:31:03 +0530

    test1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -125,6 +125,20 @@ public class Application extends Controller {
 	   Logger.info(" ====> "+state+" "+district+" "+city+" "+post);
 	   return ok();
    }
-   
+   public Result saveLocality4(String state,String district,String city,String post) {
+	   IndiaDetails details = new IndiaDetails();
+	   details.state = state;
+	   details.district = district;
+	   details.city = city;
+	   details.locality = post;
+	   details.save();
+	   
+	   
+	   
+	   
+	   
+	   Logger.info(" ====> "+state+" "+district+" "+city+" "+post);
+	   return ok();
+   }
    
 }
