a5f288688b987221ed8eab14741bca91aa09fc8c
Vamshi <vamshikrishnagoud.m@gmail.com>
2016-03-28 11:45:53 +0530

    space added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,7 +18,6 @@ import views.html.index;
 public class Application extends Controller {
 
     public Result index() {
- 
         return ok(index.render("Your new application is ready."));
     }
    public Result getSource(String state){
